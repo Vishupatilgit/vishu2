84e0f6b57279f8ed360a9f7fa21c62ea33a10dcf
Vishwa <Vishwa@LAPTOP-ACFB8J0B>
2022-09-10 16:55:22 +0530

    First commit by vishu for selenium project

---
+++ src/test/java/demo/sample.java
@@ -0,0 +1,20 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class sample 
+{
+	@Test
+	public void m1()
+	{
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Viswajit\\Downloads"
+				+ "\\chromedriver_win32\\chromedriver.exe");
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		
+	}
+
+}
